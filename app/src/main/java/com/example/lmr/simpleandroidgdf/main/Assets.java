e939aa261ed84ac4812cd4db19cb90246cbb6486
smsylmr <lin8023ke@sina.com>
2017-02-22 21:23:32 +0800

    添加资源，完成游戏循环

--- app/src/main/java/com/example/lmr/simpleandroidgdf/main/Assets.java
+++ app/src/main/java/com/example/lmr/simpleandroidgdf/main/Assets.java
@@ -5,6 +5,9 @@ import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.SoundPool;
 
+import com.example.lmr.simpleandroidgdf.animation.Animation;
+import com.example.lmr.simpleandroidgdf.animation.Frame;
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -14,9 +17,38 @@ import java.io.InputStream;
 
 public class Assets {
     private static SoundPool soundPool;
-    public static Bitmap welcome;
+    public static Bitmap welcome,block,cloud1,cloud2,duck,grass,jump,run1,run2,run3,run4,run5,scoreDown,
+            score,startDown,start;
+    public static Animation runAnim;
+    public static int hitID,onjumpID;
+
     public static void load(){
         welcome=loadBitmap("welcome.png",false);
+        block=loadBitmap("block.png",false);
+        cloud1=loadBitmap("cloud1.png",true);
+        cloud2=loadBitmap("cloud2.png",true);
+        duck=loadBitmap("duck.png",true);
+        grass=loadBitmap("grass.png",false);
+        jump=loadBitmap("jump.png",false);
+        run1=loadBitmap("run_anim1.png",true);
+        run2=loadBitmap("run_anim2.png",true);
+        run3=loadBitmap("run_anim3.png",true);
+        run4=loadBitmap("run_anim4.png",true);
+        run5=loadBitmap("run_anim5.png",true);
+        scoreDown=loadBitmap("score_button_down.png",true);
+        score=loadBitmap("score_button.png",true);
+        startDown=loadBitmap("start_button_down.png",true);
+        start=loadBitmap("start_button.png",true);
+
+        Frame f1=new Frame(run1,.1f);
+        Frame f2=new Frame(run2,.1f);
+        Frame f3=new Frame(run3,.1f);
+        Frame f4=new Frame(run4,.1f);
+        Frame f5=new Frame(run5,.1f);
+        runAnim=new Animation(f1,f2,f3,f4,f5,f3,f2);
+
+        hitID=loadSound("hit.wav");
+        onjumpID=loadSound("onjump.wav");
     }
     private static Bitmap loadBitmap(String filename,boolean transparency){
         InputStream inputStream=null;
--- app/src/main/java/com/example/lmr/simpleandroidgdf/state/MenuState.java
+++ app/src/main/java/com/example/lmr/simpleandroidgdf/state/MenuState.java
@@ -1,18 +1,29 @@
 package com.example.lmr.simpleandroidgdf.state;
 
+import android.graphics.Rect;
+import android.util.Log;
 import android.view.MotionEvent;
 
 import com.example.lmr.simpleandroidgdf.main.Assets;
 import com.example.lmr.simpleandroidgdf.util.Painter;
+import com.example.lmr.simpleandroidgdf.util.UIButton;
 
 /**
  * Created by LMR on 2017/2/22.
  */
 
 public class MenuState extends State  {
+//    private Rect playRect;
+//    private Rect scoreRect;
+//
+//    private boolean playDown=false;
+//    private boolean scoreDown=false;
+    private UIButton playButton,scoreButton;
+
     @Override
     public void init() {
-
+        playButton=new UIButton(316,227,484,286,Assets.start,Assets.startDown);
+        scoreButton=new UIButton(316,300,484,359,Assets.score,Assets.scoreDown);
     }
 
     @Override
@@ -22,11 +33,29 @@ public class MenuState extends State  {
 
     @Override
     public void render(Painter painter) {
+
         painter.drawImage(Assets.welcome,0,0);
+        playButton.render(painter);
+        scoreButton.render(painter);
     }
 
     @Override
     public boolean onTouch(MotionEvent e, int scaledX, int scaledY) {
-        return false;
+        if(e.getAction()==MotionEvent.ACTION_DOWN){
+           playButton.onTouchDown(scaledX,scaledY);
+            scoreButton.onTouchDown(scaledX,scaledY);
+            }
+        if(e.getAction()==MotionEvent.ACTION_UP){
+            if(playButton.isPressed(scaledX,scaledY)){
+                playButton.cancel();
+                setCurrentState(new PlayState());
+            }else if(scoreButton.isPressed(scaledX,scaledY)){
+                scoreButton.cancel();
+            }else {
+                playButton.cancel();
+                scoreButton.cancel();
+            }
+        }
+        return true;
     }
 }
